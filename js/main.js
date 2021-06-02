859f4df5fd6ba169903d86a3444f07da810f7993
IlariaGalliano <ilaria.galliano7@gmail.com>
2021-06-02 22:09:39 +0200

    Starbucks
    Simple landing page with HTML CSS and JS

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Functions */ 
+function imgSlider(e){
+    document.querySelector('.starbucks').src = e;
+}
+
+function circleColor(c){
+    const circle = document.querySelector('.circle');
+    circle.style.background = c;
+}
